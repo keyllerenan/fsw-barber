f635e21256e7da9094fa8072c99b87dd1ba0e0e3
keyllerenan <keyllerenan@gmail.com>
2024-08-14 22:33:52 -0300

    feat: add BookingSummary component

--- app/_components/booking-summary.tsx
+++ app/_components/booking-summary.tsx
@@ -1,6 +1,53 @@
+import { format } from "path"
+import { Card, CardContent } from "./ui/card"
+import { Barbershop, BarbershopService } from "@prisma/client"
+import { ptBR } from "date-fns/locale"
+
 // TODO: Create a BookingSummary component that displays the booking summary details.
-const BookingSummary = () => {
-    return <></>
-  }
-  
+
+interface BookingSummaryProps {
+  service: Pick<BarbershopService, "name" | "price">
+  barbershop: Pick<Barbershop, 'name'>
+  selecteDate: Date
+}
+
+const BookingSummary = ({
+  service,
+  barbershop, 
+  selectedDate,
+}: BookingSummaryProps) => {
+    return ( 
+    <Card>
+    <CardContent className="space-y-3 p-3">
+      <div className="flex items-center justify-between">
+        <h2 className="font-bold">{service.name}</h2>
+        <p className="text-sm font-bold">
+          {Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(Number(service.price))}
+        </p>
+      </div>
+
+      <div className="flex items-center justify-between">
+        <h2 className="text-sm text-gray-400">Data</h2>
+        <p className="text-sm">
+          {format(selectedDate, "d 'de' MMMM", {
+            locale: ptBR,
+            })}
+        </p>
+      </div>
+
+      <div className="flex items-center justify-between">
+        <h2 className="text-sm text-gray-400">Horário</h2>
+        <p className="text-sm">{format(selectedDate, "HH:mm")}</p>
+      </div>
+
+      <div className="flex items-center justify-between">
+        <h2 className="text-sm text-gray-400">Barbearia</h2>
+        <p className="text-sm">{barbershop.name}</p>
+      </div>
+    </CardContent>
+  </Card>
+)}
   export default BookingSummary
